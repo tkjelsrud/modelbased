6da262b728021824b39e828c0474cfe7ce3b9728
tkjelsrud <tkjelsrud@gmail.com>
2017-03-08 13:47:28 +0100

    Updated modelbased-lib.js

--- modelbased-lib.js
+++ modelbased-lib.js
@@ -15,6 +15,6 @@ function Node(id, label, x, y) {
 
 function createTestScope() {
   cscope = new Scope("Test.1", 0, [new Node("N1", "Test node", 100, 100), new Node("N2", "Test node2", 200, 100)]);
-  scope.size.x = 10;
-  scope.size.y = 10;
+  //scope.size.x = 10;
+  //scope.size.y = 10;
 }
