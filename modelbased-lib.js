ac14bbb2760cd1bf0f149288b9ae5366ca0c855d
tkjelsrud <tkjelsrud@gmail.com>
2017-03-14 11:50:20 +0100

    Updated modelbased-lib.js

--- modelbased-lib.js
+++ modelbased-lib.js
@@ -62,8 +62,8 @@ function Draw() {
     for(i = 0; i < cscope.nodes.length; i++) {
       n = cscope.nodes[i];
       cscope.nodes[i].vid = "n_" + n.id;
-      label = (n.props["label"] ? n.props["label"] : "");
-      d = $("<div id=\"n_" + n.id + "\" class=\"node\" style=\"left:" + n.x + "px;top:" + n.y + "px;\">" + n.props["label"] + "<div class=\"tags\"><ul><li>123</li><li>234</li></ul></div></div>");
+      label = ("label" in n.props? n.props["label"] : "");
+      d = $("<div id=\"n_" + n.id + "\" class=\"node\" style=\"left:" + n.x + "px;top:" + n.y + "px;\">" + label + "<div class=\"tags\"><ul><li>123</li><li>234</li></ul></div></div>");
       $(d).draggable();
       $(d).click(function(ev) {
         ShowPopup(this.id);
